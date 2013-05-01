9fafa7f6daca06a3fbef01018c70ab5d935a51c2
Zenobius Jiricek <zeno@zibmedia.com.au>
2013-05-01 17:22:08 +0930

    return correct http error when user lacks required permissions

--- guardianpie/resources.py
+++ guardianpie/resources.py
@@ -19,12 +19,16 @@ class PermissionResource(ModelResource):
 
     def override_urls(self):
         return [
-            surl(r"^<resource_name={name}>/model/<model:s>{slash}$".format(**self._meta.urlargs), self.wrap_view('get_object_list'), name="api_getlist"),
-            surl(r"^<resource_name={name}>/model/<model:s>/<pk:#>{slash}$".format(**self._meta.urlargs), self.wrap_view('get_object_detail'), name="api_getobject"),
+            surl(r"^<resource_name={name}>/model/<applabel:s>/<model:s>{slash}$".format(**self._meta.urlargs), self.wrap_view('get_object_list'), name="api_getlist"),
+            surl(r"^<resource_name={name}>/model/<applabel:s>/<model:s>/<pk:#>{slash}$".format(**self._meta.urlargs), self.wrap_view('get_object_detail'), name="api_getobject"),
         ]
 
     def get_object_list(self, request, **kwargs):
+        # TODO return a list of applabel.model objects that the
+        # user has permissions on
         pass
 
     def get_object_detail(self, request, **kwargs):
+        # TODO return a list of permission codes that the user has for
+        # (applabel.model).objects.get(pk=kwargs.get('pk'))
         pass
